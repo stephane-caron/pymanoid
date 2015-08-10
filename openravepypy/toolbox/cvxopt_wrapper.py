67cf82042dd3efd180796a19ca74f0d93f797ec9
Stephane Caron <caron@ynl.t.u-tokyo.ac.jp>
2015-08-10 19:09:45 +0900

    wrapper to cvxopt.solvers.qp (addresses the symmetry issue)

---
+++ openravepypy/toolbox/cvxopt_wrapper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Stephane Caron <caron@phare.normalesup.org>
+#
+# This file is part of openravepypy.
+#
+# openravepypy is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# openravepypy is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# openravepypy. If not, see <http://www.gnu.org/licenses/>.
+
+
+import cvxopt
+import cvxopt.solvers
+
+from numpy import array
+
+cvxopt.solvers.options['show_progress'] = False  # disable cvxopt output
+
+
+def cvxopt_solve_qp(P, q, G=None, h=None, A=None, b=None):
+    P_sym = .5 * (P + P.T)   # necessary for CVXOPT 1.1.7
+    #
+    # CVXOPT 1.1.7 only considers the lower entries of P
+    # so we need to project on the symmetric part beforehand,
+    # otherwise a wrong cost function will be used
+    #
+    M = cvxopt.matrix
+    args = [M(P_sym), M(q)]
+    if G is not None:
+        args.extend([M(G), M(h)])
+        if A is not None:
+            args.extend([M(A), M(b)])
+    x = cvxopt.solvers.qp(*args)['x']
+    return array(x).reshape((P.shape[1],))
