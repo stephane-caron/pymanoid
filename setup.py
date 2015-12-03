5fe195f82ae4ee513cf521e88f3f0d1967513575
Hervé Audren <h.audren@gmail.com>
2015-12-03 17:53:28 +0900

    Add pymanoid.toolbox to the install package list

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 
-setup(name='pymanoid', version='0.4.2', packages=['pymanoid'])
+setup(name='pymanoid', version='0.4.2', packages=['pymanoid', 'pymanoid.toolbox'])
