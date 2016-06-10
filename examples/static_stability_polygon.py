077e70df79c0cf7f537ac22fed84986131d45c87
Stephane Caron <stephane.caron@normalesup.org>
2016-06-10 19:47:41 +0200

    new example for static-equilibrium COM polygon

--- examples/static_stability_polygon.py
+++ examples/static_stability_polygon.py
@@ -18,215 +18,155 @@
 # You should have received a copy of the GNU General Public License along with
 # pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
+
 import IPython
-import cdd
+import numpy
 import openravepy
-import pylab
+import os.path
 import pymanoid
 import thread
+import threading
 import time
 
-from numpy import array, cross, dot, eye, hstack, vstack, zeros
-from pymanoid.toolbox import norm
-from scipy.linalg import block_diag
+
+dt = 3e-2  # [s]
+env_file = './openrave_models/JVRC-1/env.xml'
+env_lock = threading.Lock()
+polygon_handle = None
+z_polygon = 2.
+
+# IK settings
+qd_lim = 10.
+K_doflim = 5.
+G_com = 1. / dt
+G_contact = 0.9 / dt
+w_link = 100.
+w_com = 005.
+w_reg = 001.
 
 
-foot_half_height = 0.01  # [m]
-foot_half_length = 0.11  # [m]
-foot_half_width = 0.07   # [m]
-
-init_left_foot_pos = [0.68546862, -0.20824458, 0.32014298]
-init_left_foot_rpy = [0.11566877, 0.26659006, -3.11573395]
-init_right_foot_pos = [-0.37655576, 0.24925528, 0.36054255]
-init_right_foot_rpy = [0.19088011, -0.20956477, -0.0788266]
-
-
-class Contact(pymanoid.Box):
-
-    def __init__(self, env, pos, rpy, fric_coeff):
-        self.env = env
-        self.fric_coeff = fric_coeff
-        super(Contact, self).__init__(
-            env, X=foot_half_length, Y=foot_half_width, Z=foot_half_height,
-            color='r', pos=pos, rpy=rpy)
-
-    @property
-    def T(self):
-        T = super(Contact, self).T
-        n = T[0:3, 2]
-        T[0:3, 3] += self.Z * n
-        return T
-
-    @property
-    def pose(self):
-        pose = super(Contact, self).pose
-        pose[4:] += self.Z * self.n   # self.n calls self.T
-        return pose
-
-    @property
-    def contact_points(self):
-        c1 = dot(self.T, array([+self.X, +self.Y, -self.Z, 1.]))[:3]
-        c2 = dot(self.T, array([+self.X, -self.Y, -self.Z, 1.]))[:3]
-        c3 = dot(self.T, array([-self.X, -self.Y, -self.Z, 1.]))[:3]
-        c4 = dot(self.T, array([-self.X, +self.Y, -self.Z, 1.]))[:3]
-        return [c1, c2, c3, c4]
-
-    @property
-    def gaw_face_local(self):
-        """Ground-applied wrench cone, H-representation, local frame."""
-        X, Y = self.X, self.Y
-        mu = self.fric_coeff
-        grw_face = array([  # Ground Reaction Wrench Cone
-            # fx  fy              fz  taux tauy tauz
-            [-1,   0,            -mu,    0,   0,   0],
-            [+1,   0,            -mu,    0,   0,   0],
-            [0,   -1,            -mu,    0,   0,   0],
-            [0,   +1,            -mu,    0,   0,   0],
-            [0,    0,             -Y,   -1,   0,   0],
-            [0,    0,             -Y,   +1,   0,   0],
-            [0,    0,             -X,    0,  -1,   0],
-            [0,    0,             -X,    0,  +1,   0],
-            [-Y,  -X,  -(X + Y) * mu,  +mu,  +mu,  -1],
-            [-Y,  +X,  -(X + Y) * mu,  +mu,  -mu,  -1],
-            [+Y,  -X,  -(X + Y) * mu,  -mu,  +mu,  -1],
-            [+Y,  +X,  -(X + Y) * mu,  -mu,  -mu,  -1],
-            [+Y,  +X,  -(X + Y) * mu,  +mu,  +mu,  +1],
-            [+Y,  -X,  -(X + Y) * mu,  +mu,  -mu,  +1],
-            [-Y,  +X,  -(X + Y) * mu,  -mu,  +mu,  +1],
-            [-Y,  -X,  -(X + Y) * mu,  -mu,  -mu,  +1]])
-        gaw_face = grw_face
-        gaw_face[:, (2, 3, 4)] *= -1  # oppose local Z-axis
-        return gaw_face
-
-    @property
-    def gaw_face_world(self):
-        """Ground-applied wrench cone, H-representation, world frame."""
-        return dot(self.gaw_face_local, block_diag(self.R.T, self.R.T))
-
-
-def project_cdd_output(V, D):
-    origin = array([0., 0., 0.])
-    normal = array([0., 0., 1.])
-    assert norm(cross(normal, [0., 0., 1.])) < 1e-10
-    vertices, rays = [], []
-    for i in xrange(V.shape[0]):
-        if V[i, 0] == 1:  # 1 = vertex, 0 = ray
-            p = dot(D, V[i, 1:])
-            vertices.append([p[0], p[1], origin[2]])
-        else:
-            r = dot(D, V[i, 1:])
-            rays.append([r[0], r[1], origin[2]])
-    return vertices, rays
-
-
-class ContactSet(object):
-
-    def __init__(self, env, mass=10.):
-        self.contacts = []
-        self.env = env
-        self.mass = mass
-
-    @property
-    def nb_contacts(self):
-        return len(self.contacts)
-
-    def add(self, pos, rpy, fric_coeff=0.5):
-        self.contacts.append(Contact(self.env, pos, rpy, fric_coeff))
-
-    def compute_gaw_to_gi_matrix(self):
-        """
-        Compute the conversion matrix from ground-applied wrenches to the
-        gravito-inertial wrench.
-        """
-        N_gi = zeros((6, 6 * self.nb_contacts))
-        for i, contact in enumerate(self.contacts):
-            x, y, z = contact.p
-            Ni = array([
-                [1, 0, 0, 0, 0, 0],
-                [0, 1, 0, 0, 0, 0],
-                [0, 0, 1, 0, 0, 0],
-                [0, -z, y, 1, 0, 0],
-                [z, 0, -x, 0, 1, 0],
-                [-y, x, 0, 0, 0, 1]])
-            N_gi[:, (6 * i):(6 * (i + 1))] = Ni
-        return N_gi
-
-    def compute_static_stability_area(self, debug=True):
-        t0 = time.time()
-        W_gi = self.compute_gaw_to_gi_matrix()
-
-        # Inequalities:  A [GAW_1 GAW_2 ...] <= 0
-        A = block_diag(*[c.gaw_face_world for c in self.contacts])
-        b = zeros((A.shape[0], 1))
-
-        # Equalities:  C [GAW_1 GAW_2 ...] + d == 0
-        C = W_gi[(0, 1, 2, 5), :]
-        d = -array([0, 0, -self.mass * 9.81, 0])
-
-        # H-representation: b - A x >= 0
-        # ftp://ftp.ifor.math.ethz.ch/pub/fukuda/cdd/cddlibman/node3.html
-        # input to cdd.Matrix is [b, -A]
-        M = cdd.Matrix(hstack([b, -A]), number_type='float')
-        M.rep_type = cdd.RepType.INEQUALITY
-        M.extend(hstack([d.reshape((4, 1)), C]), linear=True)
-        P = cdd.Polyhedron(M)
-        V = array(P.get_generators())
-        if V.shape[0] < 1:
-            return []
-
-        if debug:
-            assert all(dot(A, V[1, 1:]) < 1e-2), dot(A, V[1, 1:])
-            assert norm(dot(C, V[1, 1:]) + d) < 1e-5
-
-        # COM position from GAW:  [pGx, pGy] = D * [GAW_1 GAW_2 ...]
-        D = 1. / (-self.mass * 9.81) * vstack([-W_gi[4, :], +W_gi[3, :]])
-        vertices, _ = project_cdd_output(V, D)
-        if debug:
-            print "Static stability polygon (%d vertices) computed in %d ms" \
-                % (len(vertices), int(1000 * (time.time() - t0)))
-        return vertices
-
-    def draw_static_stability_area(self, plot_type=6, alpha=0.5):
-        vertices = self.compute_static_stability_area()
-        self.com_polygon = vertices
-        if not vertices:
-            return []
-        handle = pymanoid.draw_polygon(
-            self.env, vertices, array([0., 0., 1.]),
-            plot_type=plot_type, color=(0., 0.5, 0., alpha))
-        return [handle]
-
-
-def update_thread(dt=1e-2):
-    T = [eye(4) for contact in contact_set.contacts]
-    gui_handles = None
+def run_ik_thread():
     while True:
-        for i, contact in enumerate(contact_set.contacts):
-            if pylab.norm(T[i] - contact.T) > 1e-4:
-                #
-                # NB: cdd will fail to instantiate polyhedra if transforms
-                # contain small values such as 1e-8, so we round them off.
-                #
-                contact.set_transform(contact.T.round(5))
-                new_handles = contact_set.draw_static_stability_area()
-                gui_handles = new_handles
-                T[i] = contact.T.copy()
+        env_lock.acquire()
+        robot.step_ik(dt)
+        env_lock.release()
         time.sleep(dt)
-    return gui_handles   # avoid lint warning :p
 
 
-if __name__ == '__main__':
+def run_forces_thread():
+    handles = []
+    while True:
+        env_lock.acquire()
+        try:
+            support = contacts.find_static_supporting_forces(
+                outbox.p, robot.mass)
+            handles = [pymanoid.draw_force(env, c, fc) for (c, fc) in support]
+        except Exception as e:
+            print "Force computation failed:", e
+            print "Did you move the target COM (blue box) out of the polygon?\n"
+        env_lock.release()
+        time.sleep(dt)
+    return handles
+
+
+def recompute_polygon():
+    global polygon_handle
+    vertices = contacts.compute_static_equilibrium_area(robot.mass)
+    polygon_handle = pymanoid.draw_polygon(
+        env, [(x[0], x[1], outbox.z) for x in vertices],
+        n=[0, 0, 1], color=(0.5, 0., 0.5, 0.5))
+
+
+def generate_posture(robot, contacts, com_target):
+    active_dofs = robot.chest_dofs + robot.free_dofs + robot.left_arm_dofs + \
+        robot.right_arm_dofs + robot.left_leg_dofs + robot.right_leg_dofs
+    robot.set_active_dofs(active_dofs)
+    robot.init_ik(qd_lim=qd_lim, K_doflim=K_doflim)
+    robot.add_com_objective(com_target, G_com, w_com)
+    robot.add_contact_objective(
+        robot.left_foot, contacts['left_foot'], G_contact, w_link)
+    robot.add_contact_objective(
+        robot.right_foot, contacts['right_foot'], G_contact, w_link)
+    if 'left_hand' in contacts:
+        robot.add_contact_objective(
+            robot.left_hand, contacts['left_hand'], G_contact, w_link)
+    robot.add_velocity_regularization(w_reg)
+    robot.solve_ik(dt, conv_tol=1e-4, max_it=200, debug=False)
+
+
+if __name__ == "__main__":
+    if not os.path.isfile(env_file):
+        print "Error opening file %s" % env_file
+        print "                                                                "
+        print "For this example, you need to clone the models repository:      "
+        print "                                                                "
+        print "    git clone https://github.com/stephane-caron/openrave_models "
+        print "                                                                "
+        print "and link it in this folder (or update `env_file` in the script)."
+        print "                                                                "
+        exit(-1)
+
     env = openravepy.Environment()
+    env.Load(env_file)
     env.SetViewer('qtcoin')
     viewer = env.GetViewer()
-    viewer.SetBkgndColor([.7, .7, .9])
-    viewer.SetCamera([
-        [0, 0, -1, 3],
-        [1, 0, 0, 0.1],
-        [0, -1, 0, 0.5],
-        [0, 0, 0, 1]])
-    contact_set = ContactSet(env)
-    contact_set.add(pos=init_left_foot_pos, rpy=init_left_foot_rpy)
-    contact_set.add(pos=init_right_foot_pos, rpy=init_right_foot_rpy)
-    thread.start_new(update_thread, ())
+    viewer.SetBkgndColor([.6, .6, .8])
+    viewer.SetCamera(numpy.array([
+        [0.60587192, -0.36596244,  0.70639274, -2.4904027],
+        [-0.79126787, -0.36933163,  0.48732874, -1.6965636],
+        [0.08254916, -0.85420468, -0.51334199,  2.79584694],
+        [0.,  0.,  0.,  1.]]))
+    robot = pymanoid.robots.JVRC1(env)
+    robot.set_transparency(0.25)
+
+    contacts = pymanoid.ContactSet({
+        'left_foot': pymanoid.Contact(
+            env,
+            X=0.2,
+            Y=0.1,
+            pos=[0.20, 0.15, 0.1],
+            rpy=[0, 0, 0],
+            friction=0.5,
+            visible=True),
+        'right_foot': pymanoid.Contact(
+            env,
+            X=0.2,
+            Y=0.1,
+            pos=[-0.2, -0.195, 0.],
+            rpy=[0, 0, 0],
+            friction=0.5,
+            visible=True),
+        'left_hand': pymanoid.Contact(
+            env,
+            X=0.2,
+            Y=0.1,
+            pos=[0.45, 0.46, 0.96],
+            rpy=[0., -0.8, 0.],
+            friction=0.5,
+            visible=True)
+    })
+
+    outbox = pymanoid.Cube(env, 0.02, [0, 0, z_polygon], color='b')
+    generate_posture(robot, contacts, com_target=[0.05,  0.04,  0.90])
+    recompute_polygon()
+
+    print ""
+    print "In this example, we display the static-equilibrium COM polygon"
+    print "(in magenta) for a given set of contacts."
+    print ""
+    print "You can move contacts by selecting them in the OpenRAVE GUI."
+    print "The robot IK is servoed to their positions. Type:"
+    print ""
+    print "    recompute_polygon()"
+    print ""
+    print "to recompute the COM polygon after moving contacts."
+    print ""
+    print "To illustrate the validity of this polygon, contact forces are"
+    print "computed that support the equilibrium position represented by"
+    print "the blue box (which acts like a COM position). Try moving this"
+    print "box around, and see what happens when it exits the polygon."
+    print ""
+
+    thread.start_new_thread(run_ik_thread, ())
+    thread.start_new_thread(run_forces_thread, ())
     IPython.embed()
