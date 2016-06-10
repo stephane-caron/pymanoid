edcd277204ef5c5f6dbdf4b4240c0fac98fe7313
Stephane Caron <stephane.caron@normalesup.org>
2016-06-10 18:36:32 +0200

    moar complete setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
+# -*- coding: utf-8 -*-
+
 from distutils.core import setup
 
-setup(name='pymanoid', version='0.4.2',
-      packages=['pymanoid', 'pymanoid.robots', 'pymanoid.toolbox'])
+setup(
+    name='pymanoid',
+    version='0.4.2',
+    description="Python library for humanoid robotics using OpenRAVE",
+    url="https://github.com/stephane-caron/pymanoid",
+    author="Stéphane Caron",
+    author_email="stephane.caron@normalesup.org",
+    license="GPL",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.7'],
+    packages=['pymanoid', 'pymanoid.robots', 'pymanoid.toolbox']
+)
