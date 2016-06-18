fb36b71121737e85a737f8a66468a9e5dd254be2
Stephane Caron <stephane.caron@normalesup.org>
2016-06-18 18:22:09 +0200

    missing header for previous commit

---
+++ pymanoid/env.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Stephane Caron <stephane.caron@normalesup.org>
+#
+# This file is part of pymanoid.
+#
+# pymanoid is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# pymanoid is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# pymanoid. If not, see <http://www.gnu.org/licenses/>.
+
+
+import openravepy
+
+__env__ = None
+
+
+def get_env():
+    return __env__
+
+
+def register_env(env):
+    global __env__
+    __env__ = env
+
+
+def init(env_file=None):
+    env = openravepy.Environment()
+    if env_file:
+        env.Load(env_file)
+    register_env(env)
+    env.GetPhysicsEngine().SetGravity([0, 0, -9.81])
+    env.SetViewer('qtcoin')
+    viewer = env.GetViewer()
+    viewer.SetBkgndColor([.6, .6, .8])
+    viewer.SetCamera([  # default camera in frontal plane
+        [0.,  0., 1., -3.],
+        [-1., 0., 0.,  0.],
+        [0., -1., 0.,  0.7],
+        [0.,  0., 0.,  1.]])
