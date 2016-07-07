c261eaf0748b84d5048cbc2869fd9e7802f8447d
Stephane Caron <stephane.caron@normalesup.org>
2016-07-08 01:09:29 +0200

    qpOASES: allowed for empty bounds, increased WSR

--- pymanoid/qp/backend_qpoases.py
+++ pymanoid/qp/backend_qpoases.py
@@ -19,17 +19,18 @@
 # pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
 
-from qpoases import PyQProblem as QProblem
-from qpoases import PyQProblemB as QProblemB
+from numpy import array, hstack, ones, vstack, zeros
 from qpoases import PyOptions as Options
 from qpoases import PyPrintLevel as PrintLevel
-from numpy import array, hstack, ones, vstack, zeros
+from qpoases import PyQProblem as QProblem
+from qpoases import PyQProblemB as QProblemB
+from qpoases import PyReturnValue as ReturnValue
+from warnings import warn
 
 
 __infty = 1e10
 options = Options()
 options.printLevel = PrintLevel.NONE
-nb_wsr = array([10])  # number of working set recalculations
 
 
 def solve_qp(P, q, G=None, h=None, A=None, b=None, warm_start=None):
@@ -43,27 +44,37 @@ def solve_qp(P, q, G=None, h=None, A=None, b=None, warm_start=None):
             G * x <= h
             A * x == b
 
+        .. NOTE::
+
+            This function allows empty bounds (lb, ub, lbA or ubA). This was
+            provisioned by the C++ API but not by the Python API of qpOASES (as
+            of version 3.2.0). Be sure to update the Cython file (qpoases.pyx)
+            to convert ``None`` to the null pointer, as done e.g. in
+            https://github.com/stephane-caron/qpOASES/commit/207996802f33da2375dd2db5cf58a977ac2bb0d2
+
     """
     n = P.shape[0]
-    lb = -__infty * ones(P.shape[0])
-    ub = +__infty * ones(P.shape[0])
+    lb, ub = None, None
+    nb_wsr = array([100])  # number of working set recalculations
     has_cons = G is not None or A is not None
     if G is not None and A is None:
         C = G
-        lb_C = -__infty * ones(h.shape[0])
+        lb_C = None  # NB:
         ub_C = h
     elif G is None and A is not None:
         C = vstack([A, A])
         lb_C = h
         ub_C = h
-    else:  # G is not None and A is not None:
+    elif G is not None and A is not None:
         C = vstack([G, A, A])
         lb_C = hstack([-__infty * ones(h.shape[0]), b])
         ub_C = hstack([h, b])
     if has_cons:
         qp = QProblem(n, C.shape[0])
         qp.setOptions(options)
-        qp.init(P, q, C, lb, ub, lb_C, ub_C, nb_wsr)
+        return_value = qp.init(P, q, C, lb, ub, lb_C, ub_C, nb_wsr)
+        if return_value == ReturnValue.MAX_NWSR_REACHED:
+            warn("qpOASES reached the maximum number of WSR (%d)" % nb_wsr[0])
     else:
         qp = QProblemB(n)
         qp.setOptions(options)
