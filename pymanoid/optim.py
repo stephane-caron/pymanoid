0a189d0f4ae82c8921cb93266bf3b29169a65e79
Stéphane Caron <stephane.caron@normalesup.org>
2017-03-02 14:46:39 +0100

    added nonlinear programming to optim.py

--- pymanoid/optim.py
+++ pymanoid/optim.py
@@ -342,3 +342,257 @@ def solve_safer_qp(P, q, G, h, w_reg, w_lin, solver='mosek'):
     A2 = hstack([G, -E])
     b2 = h
     return solve_qp(P2, q2, G2, h2, A2, b2, solver=solver)[:n]
+
+"""
+Nonlinear Programming
+=====================
+"""
+
+try:
+    from casadi import MX, nlpsol, vertcat
+except ImportError:
+    print "\033[1;33m[pymanoid] Warning: CasADi not found\033[0;0m"
+
+
+class NonlinearProgram(object):
+
+    infty = 1e10
+
+    casadi_expand = True
+    """
+    Replace MX with SX expressions in problem formulation.
+
+    Note
+    ----
+    Setting this option to ``True`` seems to significantly improve computation
+    times when using IPOPT.
+    """
+
+    """
+    IPOPT options
+    =============
+    """
+
+    ipopt_fast_step_computation = 'yes'  # default: 'no'
+    """
+    Indicates if the linear system should be solved quickly.
+
+    If set to yes, the algorithm assumes that the linear system that is solved
+    to obtain the search direction, is solved sufficiently well. In that case,
+    no residuals are computed, and the computation of the search direction is a
+    little faster.
+
+    Possible values:
+        - no     [Verify solution of linear system by computing residuals.]
+        - yes    [Trust that linear systems are solved well.]
+    """
+
+    ipopt_fixed_variable_treatment = 'relax_bounds'
+    """
+    Default is "make_parameter", but "relax_bounds" seems to make computations
+    faster and numerically stabler
+    """
+
+    ipopt_linear_solver = 'ma27'
+    """
+    Linear solver used for step computations.
+    """
+
+    ipopt_max_cpu_time = 0.1  # [s]
+    """
+    Maximum number of CPU seconds.
+
+    Note
+    ----
+    This parameter corresponds to processor time, not wall time. For a CPU with
+    N cores, the former can scale as much as N times the latter.
+    """
+
+    ipopt_max_iter = 100
+    """
+    Maximum number of iterations.
+    """
+
+    ipopt_mu_strategy = "adaptive"  # default is "monotone"
+    """
+    Update strategy for barrier parameter.
+
+    Options are "monotone" (default) and "adaptive".
+
+    Note
+    ----
+    The adaptive strategy yields ~40% faster results in practice on the COP and
+    ZMP controller, while it seems to have no effect on computation times of the
+    (slower) Wrench controller.
+    """
+
+    ipopt_nlp_lower_bound_inf = -0.9 * infty
+    """
+    Any bound less or equal this value will be considered -inf (i.e. not lower
+    bounded).
+    """
+
+    ipopt_nlp_upper_bound_inf = +0.9 * infty
+    """
+    Any bound greater or this value will be considered +inf (i.e. not upper
+    bounded).
+    """
+
+    ipopt_print_level = 0  # default: 5
+    """
+    Output verbosity level.
+
+    Integer between 0 and 12.
+    """
+
+    ipopt_warm_start_init_point = 'yes'  # default: 'no'
+    """
+    Indicates whether this optimization should use a warm start initialization,
+    where values of primal and dual variables are given (e.g. from a previous
+    optimization of a related problem).
+    """
+
+    def __init__(self):
+        self.cons_exprs = []
+        self.cons_index = {}
+        self.cons_lbounds = []
+        self.cons_ubounds = []
+        self.cost_function = 0
+        self.initvals = []
+        self.solver = None
+        self.var_index = {}
+        self.var_lbounds = []
+        self.var_symbols = []
+        self.var_ubounds = []
+
+    def extend_cost(self, expr):
+        self.cost_function = self.cost_function + expr
+
+    def warm_start(self, initvals):
+        assert len(self.initvals) == len(initvals)
+        self.initvals = list(initvals)
+
+    """
+    Variables
+    =========
+    """
+
+    def new_variable(self, name, dim, init, lb, ub):
+        assert len(init) == len(lb) == len(ub) == dim
+        var = MX.sym(name, dim)
+        self.var_symbols.append(var)
+        self.initvals += init
+        self.var_index[name] = len(self.var_lbounds)
+        self.var_lbounds += lb
+        self.var_ubounds += ub
+        return var
+
+    def new_constant(self, name, dim, value):
+        return self.new_variable(name, dim, init=value, lb=value, ub=value)
+
+    def update_constant(self, name, value):
+        i = self.var_index[name]
+        for j, v in enumerate(value):
+            self.initvals[i + j] = v
+            self.var_lbounds[i + j] = v
+            self.var_ubounds[i + j] = v
+
+    def update_variable_bounds(self, name, lb, ub):
+        assert len(lb) == len(ub)
+        i = self.var_index[name]
+        for j in xrange(len(lb)):
+            self.var_lbounds[i + j] = lb[j]
+            self.var_ubounds[i + j] = ub[j]
+
+    """
+    Constraints
+    ===========
+    """
+
+    def add_constraint(self, expr, lb, ub, name=None):
+        self.cons_exprs.append(expr)
+        if name is not None:
+            self.cons_index[name] = len(self.cons_lbounds)
+        self.cons_lbounds += lb
+        self.cons_ubounds += ub
+
+    def add_equality_constraint(self, expr1, expr2, name=None):
+        diff = expr1 - expr2
+        dim = diff.shape[0]
+        assert diff.shape[1] == 1
+        zeros = [0.] * dim
+        self.add_constraint(diff, zeros, zeros, name)
+
+    def has_constraint(self, name):
+        return name in self.cons_index
+
+    def update_constraint_bounds(self, name, lb, ub):
+        i = self.cons_index[name]
+        for j in xrange(len(lb)):
+            self.cons_lbounds[i + j] = lb[j]
+            self.cons_ubounds[i + j] = ub[j]
+
+    """
+    Solver
+    ======
+    """
+
+    def create_solver(self, solver='ipopt'):
+        """
+        Create a new nonlinear solver.
+
+        Parameters
+        ----------
+        solver : string
+            Solver name. Use 'ipopt' for an interior point method or 'sqpmethod'
+            for sequential quadratic programming.
+        """
+        problem = {
+            'f': self.cost_function,
+            'x': vertcat(*self.var_symbols),
+            'g': vertcat(*self.cons_exprs)}
+        options = {}
+        if solver in ['scpgen', 'sqpmethod']:
+            options.update({
+                'qpsol': "qpoases",
+                'qpsol_options': {"printLevel": "none"},
+            })
+        elif solver == 'ipopt':
+            options.update({
+                'expand': self.casadi_expand,
+                'ipopt.fast_step_computation': self.ipopt_fast_step_computation,
+                'ipopt.fixed_variable_treatment':
+                self.ipopt_fixed_variable_treatment,
+                'ipopt.linear_solver': self.ipopt_linear_solver,
+                'ipopt.max_cpu_time': self.ipopt_max_cpu_time,
+                'ipopt.max_iter': self.ipopt_max_iter,
+                'ipopt.mu_strategy': self.ipopt_mu_strategy,
+                'ipopt.nlp_lower_bound_inf': self.ipopt_nlp_lower_bound_inf,
+                'ipopt.nlp_upper_bound_inf': self.ipopt_nlp_upper_bound_inf,
+                'ipopt.print_level': self.ipopt_print_level,
+                'ipopt.warm_start_init_point': self.ipopt_warm_start_init_point,
+                'verbose': False
+            })
+        self.solver = nlpsol('solver', solver, problem, options)
+
+    def solve(self):
+        self.res = self.solver(
+            x0=self.initvals, lbx=self.var_lbounds, ubx=self.var_ubounds,
+            lbg=self.cons_lbounds, ubg=self.cons_ubounds)
+        return self.res['x'].full().flatten()
+
+    @property
+    def iter_count(self):
+        return self.solver.stats()['iter_count']
+
+    @property
+    def optimal_found(self):
+        return self.return_status == "Solve_Succeeded"
+
+    @property
+    def return_status(self):
+        return self.solver.stats()['return_status']
+
+    @property
+    def solve_time(self):
+        return self.solver.stats()['t_wall_mainloop']
