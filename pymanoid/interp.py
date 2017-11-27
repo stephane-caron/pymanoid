9b4bf478b2bef71f3a67cb8e93c05a02543da6dc
Stéphane Caron <stephane.caron@normalesup.org>
2017-11-27 11:12:14 +0100

    License changed to LGPLv3
    See: https://github.com/stephane-caron/pymanoid/issues/10

--- pymanoid/interp.py
+++ pymanoid/interp.py
@@ -6,16 +6,17 @@
 # This file is part of pymanoid <https://github.com/stephane-caron/pymanoid>.
 #
 # pymanoid is free software: you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
 # pymanoid is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU General Public License along with
-# pymanoid. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License along
+# with pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
 from numpy import hstack, linspace
 from openravepy import InterpolateQuatSlerp as quat_slerp
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2016 Stephane Caron <stephane.caron@normalesup.org>
+# Copyright (C) 2015-2017 Stephane Caron <stephane.caron@normalesup.org>
 #
 # This file is part of pymanoid.
 #
-# pymanoid is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
+# pymanoid is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
 #
-# pymanoid is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# pymanoid is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
 # details.
 #
-# You should have received a copy of the GNU General Public License along with
-# pymanoid. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License along
+# with pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
@@ -27,13 +27,13 @@ setup(
     url="https://github.com/stephane-caron/pymanoid",
     author="Stéphane Caron",
     author_email="stephane.caron@lirmm.fr",
-    license="GPL",
+    license="LGPL",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU Lesser General Public License (LGPL)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Mathematics'],
