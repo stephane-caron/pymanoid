d3d84e06a1f454185bc41c700d9ffed1172e788e
Stéphane Caron <stephane.caron@lirmm.fr>
2018-05-02 12:57:57 +0200

    Removed unused is_positive_combination function

--- pymanoid/misc.py
+++ pymanoid/misc.py
@@ -19,9 +19,7 @@
 # along with pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
 from datetime import datetime
-from numpy import array, dot, eye, sqrt, tensordot, zeros
-
-from .qpsolvers import solve_qp
+from numpy import array, dot, sqrt, tensordot, zeros
 
 
 class AvgStdEstimator(object):
@@ -182,62 +180,6 @@ def info(msg):
     """
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S,%f")[:-3]
     print("%c[0;%d;48m%s pymanoid [INFO] %s%c[m" % (0x1B, 32, now, msg, 0x1B))
-
-
-def is_positive_combination(b, A):
-    """
-    Check if b can be written as a positive combination of lines from A.
-
-    Parameters
-    ----------
-    b : array
-        Test vector.
-    A : array
-        Matrix of line vectors to combine.
-
-    Returns
-    -------
-    is_positive_combination : bool
-        Whether :math:`b = A^T x` for some positive `x`.
-
-    Notes
-    -----
-    As an alternative implementation, one could try solving a QP minimizing
-    :math:`\\|A x - b\\|^2` (and no equality constraint), however I found that
-    the precision of the output is then quite low (~1e-1).
-    """
-    try:
-        m = A.shape[0]
-        P, q, G, h = eye(m), zeros(m), -eye(m), zeros(m)
-        x = solve_qp(P, q, G, h, A.T, b)
-        if x is None:  # optimum not found
-            return False
-    except ValueError:
-        return False
-    return norm(dot(A.T, x) - b) < 1e-10 and min(x) > -1e-10
-
-
-def is_redundant(vectors):
-    """
-    Check if a set of vectors is redundant, i.e. one of them can be written as
-    positive combination of the others.
-
-    Parameters
-    ----------
-    vectors : list of arrays
-        List of vectors to check.
-
-    Notes
-    -----
-    When using CVXOPT as QP solver, this function may print out a significant
-    number of messages "Terminated (singular KKT matrix)." in the terminal.
-    """
-    F = array(vectors)
-    all_lines = set(range(F.shape[0]))
-    for i in all_lines:
-        if is_positive_combination(F[i], F[list(all_lines - set([i]))]):
-            return True
-    return False
 
 
 def matplotlib_to_rgb(color):
