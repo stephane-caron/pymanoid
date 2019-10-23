0f5b7adaa3b44d8259c4f72fac285fb3b484fd18
Stéphane Caron <stephane.caron@lirmm.fr>
2019-10-23 18:16:04 +0900

    Update license in file headers and setup.py

--- examples/horizontal_walking.py
+++ examples/horizontal_walking.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2019 Stephane Caron <stephane.caron@lirmm.fr>
+# Copyright (C) 2015-2019 Stephane Caron <stephane.caron@lirmm.fr>
 #
 # This file is part of pymanoid <https://github.com/stephane-caron/pymanoid>.
 #
 # pymanoid is free software: you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
 # pymanoid is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-# details.
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with pymanoid. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
 """
 This example implements a walking pattern generator for horizontal walking
--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2018 Stephane Caron <stephane.caron@lirmm.fr>
+# Copyright (C) 2015-2019 Stephane Caron <stephane.caron@lirmm.fr>
 #
-# This file is part of pymanoid.
+# This file is part of pymanoid <https://github.com/stephane-caron/pymanoid>.
 #
 # pymanoid is free software: you can redistribute it and/or modify it under the
-# terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
 #
 # pymanoid is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public License
-# along with pymanoid. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# pymanoid. If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
@@ -27,13 +26,13 @@ setup(
     url="https://github.com/stephane-caron/pymanoid",
     author="Stéphane Caron",
     author_email="stephane.caron@lirmm.fr",
-    license="LGPL",
+    license="GPL",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Mathematics'],
